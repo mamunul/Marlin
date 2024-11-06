58936b7f7e88ec372064fd0e0bb489e795db5f84
Mamunul <mamunul.mazid@gmail.com>
2024-11-06 13:17:06 +0600

    xetoz: redefine f405 pins

--- Marlin/src/pins/stm32f4/pins_XETOZ_F405.h
+++ Marlin/src/pins/stm32f4/pins_XETOZ_F405.h
@@ -22,64 +22,64 @@
 //
 // Limit Switches
 //
-#define X_STOP_PIN                          PA0
-#define Y_STOP_PIN                          PA1
-#define Z_STOP_PIN                          PA2
+#define X_STOP_PIN                          PC7
+#define Y_STOP_PIN                          PB1
+#define Z_STOP_PIN                          PA15
 
 
 
 //
 // Filament runout
 //
-#define FIL_RUNOUT_PIN                      PA3
+#define FIL_RUNOUT_PIN                      PC6
 
 //
 // Z Probe (when not Z_MIN_PIN)
 //
-#define Z_MIN_PROBE_PIN                   PA4
+#define Z_MIN_PROBE_PIN                   PB9
 
 
 //
 // Steppers
 //
-#define X_STEP_PIN                          PA5
-#define X_DIR_PIN                           PA6
-#define X_ENABLE_PIN                        PA7
+#define X_STEP_PIN                          PC0
+#define X_DIR_PIN                           PC2
+#define X_ENABLE_PIN                        PC1
 
-#define Y_STEP_PIN                          PA8
-#define Y_DIR_PIN                           PA9
-#define Y_ENABLE_PIN                        PA10
+#define Y_STEP_PIN                          PB8
+#define Y_DIR_PIN                           PC5
+#define Y_ENABLE_PIN                        PC1
 
-#define Z_STEP_PIN                          PA11
-#define Z_DIR_PIN                           PA12
-#define Z_ENABLE_PIN                        PA13
+#define Z_STEP_PIN                          PB15
+#define Z_DIR_PIN                           PB10
+#define Z_ENABLE_PIN                        PC1
 
-#define E0_STEP_PIN                         PA14
-#define E0_DIR_PIN                          PA15
-#define E0_ENABLE_PIN                       PB0
+#define E0_STEP_PIN                         PA0
+#define E0_DIR_PIN                          PA2
+#define E0_ENABLE_PIN                       PC1
 
-#define E1_STEP_PIN                         PB1
-#define E1_DIR_PIN                          PB2
-#define E1_ENABLE_PIN                       PB3
+#define E1_STEP_PIN                         PC4
+#define E1_DIR_PIN                          PB0
+#define E1_ENABLE_PIN                       PC1
 
 
 //
 // Temperature Sensors
 //
-#define TEMP_0_PIN                          PB4   // See below for activation of thermistor readings
+#define TEMP_0_PIN                          PB5   // See below for activation of thermistor readings
 // #define TEMP_1_PIN                          PC1   // See below for activation of thermistor readings
-#define TEMP_BED_PIN                        PB5
+#define TEMP_BED_PIN                        PB3
 
 
 //
 // Heaters / Fans
 //
-#define HEATER_0_PIN                        PB6
+#define HEATER_0_PIN                        PB11
 // #define HEATER_1_PIN                        PA1
-#define HEATER_BED_PIN                      PB7
+#define HEATER_BED_PIN                      PC15
 
-#define FAN0_PIN                            PB8  // heater 0 fan 1
-#define FAN1_PIN                            PB9  // heater 1 fan 2
+#define FAN0_PIN                            PC14  // heater 0 fan 1
+// #define FAN1_PIN                            PB9  // heater 1 fan 2
 
 
 //
@@ -90,7 +90,7 @@
 // #define HAL_SD_MODULE_ENABLED
 #define ONBOARD_SDIO
 // #define SDIO_CLOCK                       4800000
-#define SD_DETECT_PIN                       PB10
+#define SD_DETECT_PIN                       PA8
 
 //#define SPI_EEPROM
 //#define I2C_EEPROM
@@ -117,21 +117,39 @@
 // Power Supply Control
 //
 #ifndef PS_ON_PIN
-  #define PS_ON_PIN                         PB10 // PS-ON
+  #define PS_ON_PIN                         PA4 // PS-ON
 #endif
 
+#ifndef POWER_LOSS_PIN
+  #define POWER_LOSS_PIN                    PA3   // Power Loss Detection: PWR-DET
+#endif
 
-  #define TFT_CS_PIN                 PC0
-  #define TFT_A0_PIN                 PC1
-  #define TFT_SCK_PIN                PC2
-  #define TFT_MISO_PIN               PC3
-  #define TFT_MOSI_PIN               PC4
+#define FREEZE_FEATURE
+#if ENABLED(FREEZE_FEATURE)
+  #define FREEZE_PIN  PA1   // Override the default (KILL) pin here
+  #define FREEZE_STATE LOW  // State of pin indicating freeze
+#endif
+#define KILL_PIN                            PC3
 
-  #if ENABLED(WIFISUPPORT)
-  //
-  // WIFI
-  //
+//
+// Misc. Functions
+//
+#define LED_PIN                             PB2
+#define BEEPER_PIN                          PB4
 
+
+#define TFT_CS_PIN                 PB14
+#define TFT_A0_PIN                 PB12
+#define TFT_SCK_PIN                PA5
+#define TFT_MISO_PIN               PA6
+#define TFT_MOSI_PIN               PA7
+
+// ENABLED(ESP3D_WIFISUPPORT)
+#if ENABLED(WIFISUPPORT)
+        //
+        // WIFI
+        //
+      
   /**
    *          ------
    *      RX | 8  7 | 3.3V      GPIO0  PF14 ... Leave as unused (ESP3D software configures this with a pullup so OK to leave as floating)
@@ -143,8 +161,8 @@
    */
   #define ESP_WIFI_MODULE_COM                  6  // Must also set either SERIAL_PORT or SERIAL_PORT_2 to this
   #define ESP_WIFI_MODULE_BAUDRATE      BAUDRATE  // Must use same BAUDRATE as SERIAL_PORT & SERIAL_PORT_2
-  #define ESP_WIFI_MODULE_RESET_PIN         PC5
-  #define ESP_WIFI_MODULE_ENABLE_PIN        PC6
-  #define ESP_WIFI_MODULE_GPIO0_PIN         PC7
-  #define ESP_WIFI_MODULE_GPIO2_PIN         PC14
+  #define ESP_WIFI_MODULE_RESET_PIN         -1
+  #define ESP_WIFI_MODULE_ENABLE_PIN        -1
+  #define ESP_WIFI_MODULE_GPIO0_PIN         -1
+  #define ESP_WIFI_MODULE_GPIO2_PIN         -1
 #endif
